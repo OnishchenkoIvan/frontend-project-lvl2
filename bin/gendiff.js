035115e7355e0f97b569af7a42d836816f24de42
Ivan Onishchenko <Lightsout07@mail.ru>
2022-06-14 11:14:12 +0300

    stepsecond

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');  
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .version('1.0.0');
 program.parse();
