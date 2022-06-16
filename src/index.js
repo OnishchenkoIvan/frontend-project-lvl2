4435c1f81361d4641834aeb6f9f181f325b91662
Ivan Onishchenko <Lightsout07@mail.ru>
2022-06-16 10:47:16 +0300

    eslint

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import path from 'path';
 import { readFileSync } from 'fs';
 
 const genDiff = (file1, file2) => {
-  const filePath1 = path.resolve("frontend-project-lvl2", "../__fixtures__", file1);
-  const filePath2 = path.resolve("frontend-project-lvl2", "../__fixtures__", file2);
+  const filePath1 = path.resolve('frontend-project-lvl2', '../__fixtures__', file1);
+  const filePath2 = path.resolve('frontend-project-lvl2', '../__fixtures__', file2);
   const pars1 = JSON.parse(readFileSync(filePath1));
   const pars2 = JSON.parse(readFileSync(filePath2));
   let arr = [];
@@ -13,18 +13,19 @@ const genDiff = (file1, file2) => {
   arr = arr.flat().sort();
   arr = _.sortedUniq(arr);
   let strResult = '{\n';
-  const result = arr.map((key) => {
+  arr.map((key) => {
     if (pars1[key] === pars2[key]) {
-      return strResult = (`${strResult}  ${key}: ${pars1[key]}\n`);
-    } if (pars1[key] === undefined) {
-      return strResult = (`${strResult}+ ${key}: ${pars2[key]}\n`);
-    } if (pars2[key] === undefined) {
-      return strResult = (`${strResult}- ${key}: ${pars1[key]}\n`);
-    } if (pars1[key] != pars2[key]) {
-      return strResult = (`${strResult}- ${[key]}: ${pars1[key]}\n+ ${[key]}: ${pars2[key]}\n`);
+      strResult = (`${strResult}  ${key}: ${pars1[key]}\n`);
+    } else if (pars1[key] === undefined) {
+      strResult = (`${strResult}+ ${key}: ${pars2[key]}\n`);
+    } else if (pars2[key] === undefined) {
+      strResult = (`${strResult}- ${key}: ${pars1[key]}\n`);
+    } else if (pars1[key] !== pars2[key]) {
+      strResult = (`${strResult}- ${[key]}: ${pars1[key]}\n+ ${[key]}: ${pars2[key]}\n`);
     }
+    return strResult;
   });
-  return `${strResult}}`
+  return `${strResult}}`;
 };
 
 export default genDiff;
