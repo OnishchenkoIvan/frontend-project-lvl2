74a444b0aecb7b6c7f8fc10b876dd5f07e24b569
Ivan Onishchenko <Lightsout07@mail.ru>
2022-06-20 18:58:17 +0300

    npmrc

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,17 +1,17 @@
 import { test, expect } from '@jest/globals';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
-const readFile = (ident) => fs.readFileSync(path.resolve(process.cwd(), ident), 'utf-8');
-const getFixturePath = (ident) => path.join(__dirname, '..', '__fixtures__', ident);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('test of gendiff', () => {
-  const fileName1 = 'filepath1.json';
-  const fileName2 = 'filepath2.json';
-  expect(genDiff(getFixturePath(fileName1), getFixturePath(fileName2))).toEqual(readFile(getFixturePath('expected_file.txt')));
+const readFile = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf-8');
+
+test('test JSON', () => {
+  expect(genDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json')))
+    .toBe(readFile('expected_file.txt'));
 });
